caab612799c4f22370e0aa280976039a32bbd918
DRob <Darwin.Robinson8@gmail.com>
2019-03-31 21:26:20 -0400

    New Changes

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     url(r'^load_tickers/$', views.load_tickers, name='load_tickers'),
     url(r'^data_calculations/$', views.data_calculations, name='data_calculations'),
     url(r'^custom_screener/$', views.custom_screener, name='custom_screener'),
+    url(r'^data_analysis/$', views.data_analysis, name='data_analysis'),
+    url(r'^get_volume_intraday/$', views.get_volume_intraday, name='get_volume_intraday'),
+    url(r'^load_sec_fillings/$', views.load_sec_fillings, name='load_sec_fillings'),
 ]
