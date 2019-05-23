84164c4952c31e5c20a888ffef81a552cb310023
DRob <Darwin.Robinson8@gmail.com>
2019-05-22 22:10:37 -0400

    New Changes

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls import url
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
+    path('log/', views.log, name='blog-log'),
     url(r'^load_tickers/$', views.load_tickers, name='load_tickers'),
     url(r'^data_calculations/$', views.data_calculations, name='data_calculations'),
     url(r'^custom_screener/$', views.custom_screener, name='custom_screener'),
