2676148ee114d20a2f2555874e1b3628df1f253d
DRob <Darwin.Robinson8@gmail.com>
2019-05-22 22:05:06 -0400

    New Changes

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import TickerLogger
+from .models import Ticker
 
 # Register your models here.
+admin.site.register(TickerLogger)
+admin.site.register(Ticker)
